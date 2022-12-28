4452c2151eec274c6c78b426a2f3c29ec8c0a88c
aurangzaib <aurangzaib3249@gmail.com>
2022-12-28 13:45:37 +0500

    context processor

--- Buyer/context_processors.py
+++ Buyer/context_processors.py
@@ -2,9 +2,14 @@
 from Buyer.models import Chat
 from django.db.models import Q
 def context_processor(request):
-    Pending_conversations=Chat.objects.filter(Q(conversation_id__seller=request.user) | Q(conversation_id__buyer=request.user),~Q(sender=request.user),Q(status="Pending")| Q(status="Delivered"))
-   
+    if request.user.is_authenticated:
+        Pending_conversations=Chat.objects.filter(Q(conversation_id__seller=request.user) | Q(conversation_id__buyer=request.user),~Q(sender=request.user),Q(status="Pending")| Q(status="Delivered"))
     
-    return {
-        "Pending_conversations":Pending_conversations.count()
-    }
+        
+        return {
+            "Pending_conversations":Pending_conversations.count()
+        }
+    else:
+        return {
+            "Pending_conversations":0
+        }
